a1352c7c1f4171a839fad5d986e055448fa25d0d
sinhu <tlsgn26@naver.com>
2023-07-17 22:12:44 +0900

    deepClone 만들어보기

---
+++ Variables/deepClone.js
@@ -0,0 +1,42 @@
+const OBJ1 = {
+    prop1 : {
+        prop1: {
+            prop1: "a",
+            prop2: "b",
+            prop3: "c"
+        },
+        prop2 : {
+            prop1: "a",
+            prop2: "b",
+            prop3: "c"
+        }
+    },
+    prop2: 1,
+    prop3: {
+        prop1: "a",
+        prop2: ["1", "2", "3"],
+        prop3: false
+    }
+}
+
+function deepClone(obj){
+    const cloneObj = {};
+    Object.assign(cloneObj, obj);
+
+    for (const key in obj) {
+        if(typeof obj[key] === 'object') {
+            cloneObj[key] = deepClone(obj[key]);
+        } else {
+            cloneObj[key] = obj[key];
+        }
+    }
+
+    return cloneObj;
+}
+
+const OBJ2 = deepClone(OBJ1);
+
+OBJ2.prop3.prop3 = true;
+
+console.log(OBJ1);
+console.log(OBJ2);
